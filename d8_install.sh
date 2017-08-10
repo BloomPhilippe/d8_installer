9d808b0b6386a4f0a0c4e94348950db1201e86c1
Philippe Bloom <philippe.bloom@produweb.be>
2017-08-10 08:00:05 +0200

    rename file

---
+++ d8_install.sh
@@ -0,0 +1,34 @@
+composer create-project drupal/drupal $1
+cd $1
+
+# Module qui permet de créer un export de votre base de données en code
+composer require drupal/features
+
+# Module qui permet de créer des actions qui s'execute en fonction d'un evenement
+composer require drupal/rules
+
+# Module qui permet de créer des pattern d'url
+composer require drupal/pathauto
+composer require drupal/token
+composer require drupal/token_language
+
+# Module qui permet des style d'image performant
+composer require drupal/image_effects
+
+# Module qui permet d'avoir des champs adresse et des vue en Google Maps
+composer require drupal/geolocation
+
+# Module qui permet de cloner ses contenu
+composer require drupal/entity_clone
+
+# Module qui permet d'avoir des formulaires dans des blocs
+composer require drupal/contact_block
+
+# Module qui permet de créer des champs de type formulaire
+composer require drupal/contact_storage
+
+# Module qui permet de se connecter via les réseaux sociaux
+# info : https://www.drupal.org/node/2763731
+# info : https://stackoverflow.com/questions/17166848/invalid-client-in-google-oauth2
+composer require drupal/social_api
+composer require drupal/social_auth
--- installer-drupal-8.sh
+++
@@ -1,3 +0,0 @@
-drush dl drupal-8 --drupal-project-rename=$1
-cd $1
-drush site-install --db-url=mysql://root:''@localhost/$2
